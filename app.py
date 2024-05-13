99a7fed2f4a1fe513ac11b389cd761fb7c528249
Harshita Wankhade <hwankhade29@gmail.com>
2024-05-13 11:22:21 +0530

    The web app is ready

--- app.py
+++ app.py
@@ -27,6 +27,13 @@ def predict_api():#input given in jason format which will capture inside the dat
     print(output[0])
     return jsonify(output[0])
 
+@app.route("/predict",methods=["POST"])
+def predict():
+    data=[float(x)for x in request.form.values()]#whatever values we are filling in that form we will be able to capture it bcoz all the request will be present in request object
+    final_input=scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=regmodel.predict(final_input)[0]
+    return render_template("home.html",prediction_text="The House price prediction is {}".format(output))#reder is v.imp in flask
 
 if __name__=="__main__":
     app.run(debug=True)
